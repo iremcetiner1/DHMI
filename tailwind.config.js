26fe57cc3ffa9a5783a6b62d1259a2967b4dd071
iremcetiner1 <biiremcetiner@gmail.com>
2024-08-02 15:48:33 +0300

    last2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    },
+    extend: {
+      colors: {
+        'dhmi-blue': '#095CBE',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+        
+      },
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+    },
+    
+    
   },
   plugins: [],
 }
